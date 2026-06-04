65d5cd745d7c4b0da9678cb1de66b09fc7625947
Rajtiwari07-cmd <rajsanjaytiwari07@gmail.com>
2026-06-04 09:30:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.static(__dirname));
 
 /* =========================
@@ -133,7 +131,6 @@ app.get("/download", (req, res) => {
     exec(command, (error, stdout, stderr) => {
 
         console.log(stdout);
-
         console.log(stderr);
 
         if (error) {
@@ -182,7 +179,46 @@ app.get("/download", (req, res) => {
             latestFile.name
         );
 
-        res.download(filePath);
+        res.download(
+            filePath,
+            latestFile.name,
+            (err) => {
+
+                if (err) {
+
+                    console.log(
+                        "Download error:",
+                        err
+                    );
+
+                    return;
+                }
+
+                fs.unlink(
+                    filePath,
+                    (unlinkErr) => {
+
+                        if (unlinkErr) {
+
+                            console.log(
+                                "Failed to delete file:",
+                                unlinkErr
+                            );
+
+                        } else {
+
+                            console.log(
+                                "Deleted file:",
+                                latestFile.name
+                            );
+
+                        }
+
+                    }
+                );
+
+            }
+        );
 
     });
 
